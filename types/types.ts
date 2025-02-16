11024ab9744c22e2a500e3077837c2c356475b2b
DenZaiyy <denzaiyy@outlook.fr>
2025-02-16 17:26:05 +0100

    up: types + schema prisma

--- types/types.ts
+++ types/types.ts
@@ -1,73 +1,85 @@
 interface TUser {
-    id: string
-    username: string
-    email: string
-    password: string
-    createdAt: Date
-    updatedAt: Date
+    id: string;
+    username: string;
+    email: string;
+    password: string;
+    createdAt: Date;
+    updatedAt?: Date;
 }
 
 interface TIngredient {
-    name: string,
-    unit_measure: string,
-    recipe: TRecipe
+    id: string
+    name: string;
+    image: string
+    unit_measure: string;
+    quantity: number
+    //recipe: TRecipe;
+}
+
+interface TStep {
+    id: string
+    stepNumber: number;
+    content: string;
 }
 
 interface TComment {
-    id: string
-    content: string
-    userId: string
-    createdAt: Date
-    recipe: TRecipe
+    id: string;
+    content: string;
+    user: TUser;
+    createdAt: Date;
+    //recipe: TRecipe;
 }
 
 interface TTag {
-    id: string
-    name: string
-    recipes: TTagRecipe[]
+    id: string;
+    name: string;
+    //recipes: TTagRecipe[];
 }
 
 interface TTool {
-    id: string
-    name: string
-    recipes: TToolRecipe[]
+    id: string;
+    name: string;
+    image: string
+    //recipes: TToolRecipe[];
 }
 
 interface TCategory {
-    id: string
-    name: string
-    recipes: TCategoryRecipe[]
+    id: string;
+    name: string;
+    //recipes: TRecipe[];
 }
 
-interface TRecipe  {
-    id: string
-    title: string
-    image: string
-    instructions: string
-    duration: number
-    slug: string
-    ingredients: TIngredient[]
-    comments: TComment[]
-    tags: TTagRecipe[]
-    tools: TToolRecipe[]
-    categories: TCategoryRecipe[]
-    createdAt: Date
+interface TRecipe {
+    id: string;
+    title: string;
+    image: string;
+    instructions: string;
+    duration: number;
+    difficulty: number;
+    slug: string;
+    ingredients: TIngredient[];
+    tools: TTool[];
+    steps: TStep[];
+    tags: TTag[];
+    category: TCategory;
+    comments?: TComment[];
+    createdAt: Date;
 }
 
 interface TCategoryRecipe {
-    id: string
-    category: TCategory
-    recipe: TRecipe
+    id: string;
+    category: TCategory;
+    recipe: TRecipe;
 }
 
 interface TTagRecipe {
-    id: string
-    recipe: TRecipe
-    tag: TTag
+    id: string;
+    recipe: TRecipe;
+    tag: TTag;
 }
 
 interface TToolRecipe {
-    id: string
-    tool: TTool
-    recipe: TRecipe
+    id: string;
+    tool: TTool;
+    recipe: TRecipe;
 }
