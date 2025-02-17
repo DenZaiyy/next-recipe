32eb4a9e53b0ba3f75aa483545aa685a11462f1f
DenZaiyy <denzaiyy@outlook.fr>
2025-02-17 19:21:34 +0100

    fix: rename components

--- app/recipe/page.tsx
+++ app/recipe/page.tsx
@@ -1,18 +1,19 @@
-//import { fetchRecipes } from "@/hooks/recipe/fetchRecipes";
-import { Slider } from "@/components/Slider";
-import {recipes} from "@/lib/data";
+import {fetchRecipes} from "@/hooks/recipe/fetchRecipes";
+import {RecipesSlider} from "@/components/RecipesSlider";
 import './style.css'
 
 const RecipePage = async () => {
-    //const recipes = await fetchRecipes();
+    const recipes = await fetchRecipes();
 
     return (
         <>
-            <h1 className="text-4xl text-white font-bold mb-4">
+            <h1 className="text-4xl text-foreground font-bold mb-4">
                 Latest Recipes
             </h1>
 
-            {recipes.length > 0 && <Slider recipes={recipes} />}
+            <div className={""}>
+                {recipes.length > 0 && <RecipesSlider recipes={recipes}/>}
+            </div>
         </>
     );
 };
---
+++ components/RecipesSlider.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React from "react";
+import {Swiper, SwiperSlide} from 'swiper/react';
+import 'swiper/css/bundle';
+import {EffectCoverflow, Navigation} from "swiper/modules";
+import {RecipeCard} from "@/components/RecipeCard";
+
+interface ISliderProps {
+    recipes: TRecipe[]
+}
+
+export const RecipesSlider: React.FC<ISliderProps> = ({recipes}) => {
+    return (
+        <>
+            <Swiper
+                effect={'coverflow'}
+                grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={'auto'}
+                coverflowEffect={{
+                    rotate: 20,
+                    stretch: 50,
+                    depth: 100,
+                    modifier: 1,
+                    slideShadows: true,
+                }}
+                modules={[EffectCoverflow, Navigation]}
+                spaceBetween={100}
+                navigation={true}
+                pagination={false}
+            >
+                {recipes.map((recipe: TRecipe) => (
+                    <SwiperSlide key={recipe.id}>
+                        {({isActive}) => (
+                            <RecipeCard
+                                recipe={recipe}
+                                active={isActive}
+                            />
+                        )}
+                    </SwiperSlide>))}
+            </Swiper>
+        </>
+    )
+}
