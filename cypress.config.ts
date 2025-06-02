9ebd717242d3cb3653939695a1cdaa0a7fd970eb
DenZaiyy <denzaiyy@outlook.fr>
2025-06-02 16:54:17 +0200

    fix: set base URL in Cypress config and update visit commands to use relative paths

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress"
 export default defineConfig({
 	projectId: "bqk5cf",
 	e2e: {
+		baseUrl: "http://localhost:3000",
 		defaultCommandTimeout: 10000, // 10 seconds
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,6 +1,6 @@
 describe("Homepage to recipes page", () => {
 	it("Home to recipe lists", () => {
-		cy.visit("http://localhost:3000/")
+		cy.visit("/")
 
 		cy.contains("CI/CD avec CircleCI")
 		cy.contains("Cypress")
@@ -14,9 +14,7 @@ describe("Homepage to recipes page", () => {
 
 describe("Recipe detail", () => {
 	it("Recipe detail", () => {
-		cy.visit(
-			"http://localhost:3000/recipe/quiche-de-courge-butternut-et-jambon/",
-		)
+		cy.visit("/recipe/quiche-de-courge-butternut-et-jambon/")
 		cy.get("h1").contains("Quiche de courge butternut et jambon")
 		cy.get("h2").contains("Instructions")
 		cy.get("h2").contains("Steps")
