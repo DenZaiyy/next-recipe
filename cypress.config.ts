d5a3d4ab0101aa96df898a6a1893bc4c7293365b
DenZaiyy <denzaiyy@outlook.fr>
2025-06-02 16:15:13 +0200

    fix: remove parallelism setting from Cypress node executor and simplify run command

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress"
 export default defineConfig({
 	projectId: "bqk5cf",
 	e2e: {
+		defaultCommandTimeout: 10000, // 10 seconds
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
 		},
